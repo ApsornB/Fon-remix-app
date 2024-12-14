94a66912ec26c5a47df6c79818edc6023f8cf69b
ApsornB <apsorn.bhr@rmutto.ac.th>
2024-12-14 12:04:54 +0700

    week5

---
+++ app/routes/toDolists.tsx
@@ -0,0 +1,32 @@
+export const toDoItems = [
+    {
+    id : 1,
+    title : "ตีปิงปอง",
+    created : "2024-12-14 09:46:00",
+    completed : true
+    },
+    {
+        id : 2,
+        title : "ปั่นจักรยาน",
+        created : "2024-12-14 10:40:00",
+        completed : true
+        },
+        {
+            id : 3,
+            title : "ตกปลา",
+            created : "2024-12-14 13:00:00",
+            completed : true
+            },
+            {
+                id : 4,
+                title : "ปีนเขา",
+                created : "2024-12-14 15:10:00",
+                completed : true
+                },
+                {
+                    id : 5,
+                    title : "เตะบอล",
+                    created : "2024-12-14 17:30:00",
+                    completed : true
+                    }
+];
